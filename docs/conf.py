ca140008e85166fdfcd7563b0460a92ca6a62a76
Paul Yushkevich <pyushkevich@gmail.com>
2025-03-31 08:51:56 -0400

    Added documentation

---
+++ docs/conf.py
@@ -0,0 +1,33 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'ITK-SNAP Deep Learning Segmentation Server'
+copyright = '2025, Paul A. Yushkevich'
+author = 'Paul A. Yushkevich'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "myst_parser",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode"
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+source_suffix = [".rst", ".md"]
---
+++ docs/index.rst
@@ -0,0 +1,13 @@
+.. ITK-SNAP Deep Learning Segmentation Server documentation master file, created by
+   sphinx-quickstart on Mon Mar 31 08:44:09 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+ITK-SNAP Deep Learning Segmentation Server documentation
+========================================================
+This Python package interfaces with `ITK-SNAP <http://itksnap.org/>`_ to allow fast deep learning segmentation. Currently, the package provides an interface between ITK-SNAP and `nnInteractive <https://github.com/MIC-DKFZ/nnInteractive>`_, a 3D promptable segmentation model by Isensee et al.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
